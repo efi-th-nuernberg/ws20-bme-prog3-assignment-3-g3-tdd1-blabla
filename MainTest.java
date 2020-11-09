c2fcdf7b2baa2a42857b5ab14dc8f0956a81f04b
repl.it user <replituser@example.com>
2020-11-09 15:11:54 +0000

    Assignment 3

---
+++ MainTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+  @Test
+  public void testNothing() 
+  {
+
+  }
+
+  @Test
+  public void testSomeStuff()
+  {
+    String s = "";
+    assertTrue(1 + 3 == 4);
+    assertFalse(s == null);
+    assertTrue(3 * 7 > 4 * 5);
+  }
+}
+
