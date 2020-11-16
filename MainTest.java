290b0e42755d3aa7e5487ee9c71fa2e1334f7b1f
repl.it user <replituser@example.com>
2020-11-16 16:54:51 +0000

    Test class with failure messages

---
+++ MainTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+  @Test
+  public void testNothing() 
+  {
+
+  }
+
+  @Test
+  public void testSomeStuff()
+  {
+    String s = "";
+    try {
+      assertEquals(5, 2 + 3);
+      
+    } catch (AssertionError e){
+      System.out.println("Term does not match expected result!");
+    }
+
+  try {
+    assertFalse(s == null);
+  } catch (AssertionError e){
+    System.out.println("String is null!");
+  }
+
+  try {
+    assertTrue(3 * 7 > 4 * 5);
+  } catch (AssertionError e){
+    System.out.println("First term is not greater than the second one!");
+  }
+    
+    
+    
+  }
+}
+
